ddfff08df56f3865b1c800eae7da05e24ac7e388
moncapiten <moncapiten@gmail.com>
2024-11-24 17:29:06 +0100

    making slow steamahead

--- Code/matlab/dataVisual2.m
+++ Code/matlab/dataVisual2.m
@@ -1,53 +1,58 @@
 dataPosition = '../../Data/';
 filefolder = 'caratteristiche';
-filename = 'data14-2';
-
-flagSave = false;
-
-%dataPosition = '../../Data/';
-%filename = 'data009';
-
-Rl  = 1e3;
+filename = 'data14-';
 
 
 
-% data import and creation of variance array
-rawData = readmatrix(strcat(dataPosition, filefolder, '/', filename, '.txt'));
+for j = 12:12
+%    tt = [];
+%    ch1 = [];
+%    s_ch1 = [];
+%    ch2 = [];
+%    s_ch2 = [];
 
-tt = rawData(:, 1);
-ch1 = rawData(:, 2);
-ch2 = rawData(:, 3);
+    swap1 = readmatrix(strcat(dataPosition, filefolder, '/', filename, '1.txt'));
+    swap2 = readmatrix(strcat(dataPosition, filefolder, '/', filename, '2.txt'));
+    swap3 = readmatrix(strcat(dataPosition, filefolder, '/', filename, '3.txt'));
+
+%    tt = [ tt, swap1(i, 1) ];
+
+    swap_ch1 = [ swap1(:, 2), swap2(:, 2), swap3(:, 2) ];
+    swap_ch2 = [ swap1(:, 3), swap2(:, 3), swap3(:, 3) ];
+
+    ch1 = mean(swap_ch1, 2);
+    s_ch1 = std(swap_ch1, 0, 2);
+    ch2 = mean(swap_ch2, 2);
+    s_ch2 = std(swap_ch2, 0, 2);
 
 
-%io = vo/R;
-Id = (ch1-ch2) / Rl;
-vds = ch2;
 
-plot(ch2, Id, 'o', Color = '#0027BD');
-%plot(tt, vi, 'o', Color = '#0027BD');
-%hold on
-%plot(tt, vo, 'o', Color = '#FF7F00');
+    Rl  = 994.6;
+    s_R1 = 3.9;
+
+
+
+
+
+    Id = (ch1-ch2) / Rl;
+
+    s_Id = sqrt( (s_ch1/Rl).^2 + (s_ch2/Rl).^2 + ( (ch1-ch2)*s_R1/(Rl^2) ).^2 );
+
+    errorbar(ch2, Id, 0.5*s_Id, 0.5*s_Id, 0.5*s_ch2, 0.5*s_ch2, 'o', Color = '#0027BD');
+    if j == 12
+        hold on
+    end
+
+end
+
+
 
 
 grid on
 grid minor
-%title('Amlitude and Ottset of input signal');
+title('MOSFET Output Characteristic');
 %legend('photodiode', 'LED', 'Amplitude in - 45k divider', 'Ottset in - 45k divider', Location= 'ne')
 ylabel(' I [A]')
 xlabel('Vds [V]')
 
 hold off
-
-%Il = mean( Vl) / Rl
-Ip = mean( Vp) / Rph
-%s_Il = std( Vl)
-s_Vp = std( Vp ) / mean(Vp) * Ip
-
-
-
-
-if flagSave
-    fig = gcf;
-    orient(fig, 'landscape')
-    print(fig, strcat(mediaposition, medianame, '.pdf'), '-dpdf')
-end
