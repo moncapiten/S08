9befb02ba51956eb5af3d43bb8d591190d85eff8
moncapiten <moncapiten@gmail.com>
2024-11-24 18:18:01 +0100

    made the PortaNOT.pdf plot pretty well

--- Code/matlab/dataVisual2.m
+++ Code/matlab/dataVisual2.m
@@ -1,21 +1,27 @@
+clear all;
+
 dataPosition = '../../Data/';
 filefolder = 'caratteristiche';
-filename = 'data14-';
+filename = 'data';
+
+% a color array containing 8 colors
+colors = ["#cc0001" "#fb940b" "#ffff01" "#01cc00" "#03c0c6" "#0000fe" "#762ca7" "#fe98bf"];
+names = [];
+
+Rl  = 994.6;
+s_R1 = 3.9;
 
 
 
-for j = 12:12
-%    tt = [];
-%    ch1 = [];
-%    s_ch1 = [];
-%    ch2 = [];
-%    s_ch2 = [];
 
-    swap1 = readmatrix(strcat(dataPosition, filefolder, '/', filename, '1.txt'));
-    swap2 = readmatrix(strcat(dataPosition, filefolder, '/', filename, '2.txt'));
-    swap3 = readmatrix(strcat(dataPosition, filefolder, '/', filename, '3.txt'));
+for i = 12:19
+    swapString = int2str(i);
+    names = [names, strcat( "$ V_{GS} = ", swapString(1), ".", swapString(2), "\; \mathrm{V} $") ];
 
-%    tt = [ tt, swap1(i, 1) ];
+    swap1 = readmatrix(strcat(dataPosition, filefolder, '/', filename, swapString, '-1.txt'));
+    swap2 = readmatrix(strcat(dataPosition, filefolder, '/', filename, swapString, '-2.txt'));
+    swap3 = readmatrix(strcat(dataPosition, filefolder, '/', filename, swapString, '-3.txt'));
+
 
     swap_ch1 = [ swap1(:, 2), swap2(:, 2), swap3(:, 2) ];
     swap_ch2 = [ swap1(:, 3), swap2(:, 3), swap3(:, 3) ];
@@ -25,34 +31,35 @@ for j = 12:12
     ch2 = mean(swap_ch2, 2);
     s_ch2 = std(swap_ch2, 0, 2);
 
+    Id = (ch1-ch2) / Rl * 1e3;
+    s_Id = sqrt( (s_ch1/Rl).^2 + (s_ch2/Rl).^2 + ( (ch1-ch2)*s_R1/(Rl^2) ).^2 ) * 1e3;
 
-
-    Rl  = 994.6;
-    s_R1 = 3.9;
-
-
-
-
-
-    Id = (ch1-ch2) / Rl;
-
-    s_Id = sqrt( (s_ch1/Rl).^2 + (s_ch2/Rl).^2 + ( (ch1-ch2)*s_R1/(Rl^2) ).^2 );
-
-    errorbar(ch2, Id, 0.5*s_Id, 0.5*s_Id, 0.5*s_ch2, 0.5*s_ch2, 'o', Color = '#0027BD');
-    if j == 12
+    errorbar(ch2, Id, 0.5*s_Id, 0.5*s_Id, 0.5*s_ch2, 0.5*s_ch2, '.', Color = colors(i-11), MarkerSize = 10);
+    if i == 12
         hold on
     end
 
 end
 
+%Ilod = repelen( 4.983 / Rl, length(Id) );
+Vlod = linspace(0, 4.983, 100);
+plot(Vlod, (4.983-Vlod)/Rl*1e3, '--', Color= 'black', LineWidth= 1.5);
+names = [names, "Load Line"];
 
+
+
+hold off
 
 
 grid on
 grid minor
 title('MOSFET Output Characteristic');
-%legend('photodiode', 'LED', 'Amplitude in - 45k divider', 'Ottset in - 45k divider', Location= 'ne')
-ylabel(' I [A]')
+legend(names, Location= 'ne', Interpreter = 'latex')
+%legend([ "1.2", "1.3", "1,4", "1.5", "1.6" "1.7", "1.8", "1.9"], Location= 'ne')
+ylabel(' I [mA]')
 xlabel('Vds [V]')
 
-hold off
+xlim([-0.1 5.1])
+ylim([-0.1 5.1])
+
+set(gca, 'FontSize', 14)
